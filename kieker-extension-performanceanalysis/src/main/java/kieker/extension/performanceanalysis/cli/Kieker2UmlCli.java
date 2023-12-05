e34aa71edb0bcd73c0686b02d42b01c4fc517b73
Clemens Kurz <ClemensB.Kurz@gmail.com>
2023-12-05 20:19:41 +0100

    Kieker2Uml * default output directory is now "output" to be consistent with the other cli interfaces

--- kieker-extension-performanceanalysis/src/main/java/kieker/extension/performanceanalysis/cli/Kieker2UmlCli.java
+++ kieker-extension-performanceanalysis/src/main/java/kieker/extension/performanceanalysis/cli/Kieker2UmlCli.java
@@ -22,7 +22,7 @@ public class Kieker2UmlCli {
     private Path modelPath = getDefaultModelPath();
 
     private static Path getDefaultModelPath() {
-        final String defaultModelPath = "model-output.uml";
+        final String defaultModelPath = "output/model-output.uml";
         new InputModelValidator().validate("default modelPath", defaultModelPath);
         return Paths.get(defaultModelPath);
     }
@@ -62,13 +62,6 @@ public class Kieker2UmlCli {
     private boolean recursive = false;
 
 
-    @Parameter(names = {"-o", "--output"},
-            description = "The directory to which the output is written. If this is not set, the output will be written to the current directory.",
-            converter = PathConverter.class,
-            validateWith = OutputDirectoryValidator.class
-    )
-    private Path outputDirectory = Paths.get("");
-
     public List<Path> getInputFiles() {
         return inputFiles;
     }
@@ -79,10 +72,6 @@ public class Kieker2UmlCli {
 
     public boolean isRecursive() {
         return recursive;
-    }
-
-    public Path getOutputDirectory() {
-        return outputDirectory;
     }
 
     public boolean isHelp() {
