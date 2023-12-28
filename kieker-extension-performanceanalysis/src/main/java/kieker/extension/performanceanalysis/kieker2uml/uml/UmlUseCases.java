2ef469b18121d3d99d47bfa22b32e3055924ae7e
Clemens Kurz <ClemensB.Kurz@gmail.com>
2023-12-28 19:09:07 +0100

    Uml2Lqn.etl * multiple interactions with open workload * it is now possible to have different modes for the use cases, where one use case can contain multiple interactions or * the interactions are separated between use case numbering each with their own actor

--- kieker-extension-performanceanalysis/src/main/java/kieker/extension/performanceanalysis/kieker2uml/uml/UmlUseCases.java
+++ kieker-extension-performanceanalysis/src/main/java/kieker/extension/performanceanalysis/kieker2uml/uml/UmlUseCases.java
@@ -1,12 +1,15 @@
 package kieker.extension.performanceanalysis.kieker2uml.uml;
 
+import com.beust.jcommander.Strings;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.uml2.uml.Actor;
 import org.eclipse.uml2.uml.Model;
+import org.eclipse.uml2.uml.Package;
 import org.eclipse.uml2.uml.UMLFactory;
 import org.eclipse.uml2.uml.UMLPackage;
 import org.eclipse.uml2.uml.UseCase;
 
+import java.awt.datatransfer.StringSelection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,6 +24,13 @@ public class UmlUseCases {
     public static final EClass USE_CASE_E_CLASS = UMLFactory.eINSTANCE.createUseCase().eClass();
     public static final String KIEKER_ENTRY_NAME = "'Entry'";
 
+
+    /**
+     * set by the developers different cases how the use case / interaction relation is
+     * see {@link UmlUseCases#getUseCase(String, Package, String)} method for usage.
+     */
+    public static String UC_MODE = "multi-interaction";
+
     static UseCase getUseCase(final Model model, final String useCaseName, final String traceRepresentation) {
         final org.eclipse.uml2.uml.Package dynamicView = getDynamicView(model);
         final UseCase useCase = getUseCase(useCaseName, dynamicView, traceRepresentation);
@@ -30,20 +40,40 @@ public class UmlUseCases {
     }
 
     private static UseCase getUseCase(final String useCaseName, final org.eclipse.uml2.uml.Package dynamicView, final String traceRepresentation) {
-        final List<UseCase> ucList = dynamicView.getPackagedElements().stream()
-                .filter(pe -> pe instanceof UseCase)
-                .map(uc -> (UseCase) uc)
-                .filter(uc -> uc.getName().contains(useCaseName))
-                .collect(Collectors.toList());
 
-        return ucList.stream()
-                .filter(uc -> Kieker2UmlUtil.getRepresentation(uc).map(r -> r.equals(traceRepresentation)).orElse(false))
-                .findFirst()
-                .orElseGet(() -> {
-                    final UseCase useCase = (UseCase) dynamicView.createPackagedElement(useCaseName + "-" + ucList.size(), USE_CASE_E_CLASS);
-                    Kieker2UmlUtil.setRepresentation(useCase, traceRepresentation);
-                    return useCase;
-                });
+        if (UC_MODE.equals("single-interaction")) { // each interaction has their own use-case
+            final List<UseCase> ucList = dynamicView.getPackagedElements().stream()
+                    .filter(pe -> pe instanceof UseCase)
+                    .map(uc -> (UseCase) uc)
+                    .filter(uc -> uc.getName().contains(useCaseName))
+                    .collect(Collectors.toList());
+            return ucList.stream()
+                    .filter(uc -> Kieker2UmlUtil.getRepresentation(uc).map(r -> r.equals(traceRepresentation)).orElse(false))
+                    .findFirst()
+                    .orElseGet(() -> {
+                        final UseCase useCase = (UseCase) dynamicView.createPackagedElement(useCaseName + "-" + ucList.size(), USE_CASE_E_CLASS);
+                        Kieker2UmlUtil.setRepresentation(useCase, traceRepresentation);
+                        return useCase;
+                    });
+        }
+        if (UC_MODE.equals("multi-interaction")) {
+            final List<UseCase> ucList = dynamicView.getPackagedElements().stream()
+                    .filter(pe -> pe instanceof UseCase)
+                    .map(uc -> (UseCase) uc)
+                    .filter(uc -> uc.getName().equals(useCaseName))
+                    .collect(Collectors.toList());
+            if (ucList.isEmpty()) {
+                final UseCase useCase = (UseCase) dynamicView.createPackagedElement(useCaseName , USE_CASE_E_CLASS);
+                Kieker2UmlUtil.setRepresentation(useCase, traceRepresentation);
+                return useCase;
+            }
+            if (ucList.size() > 1) {
+                final String collect = ucList.stream().map(Object::toString).collect(Collectors.joining(", "));
+                throw new RuntimeException("To many use cases. Expected was exactly one. List: " + collect);
+            }
+            return ucList.get(0);
+        }
+        throw new RuntimeException("Unable to find appropriate UseCase Mode, please contact the developers.");
     }
 
     static Actor getActor(final org.eclipse.uml2.uml.Package dynamicView, final UseCase useCase) {
